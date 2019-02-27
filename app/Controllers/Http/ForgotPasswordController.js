f21c61ab7560330045f6b81e2d8ce48d3601cbda
dvsilva <Danyllo Silva>
2019-02-27 08:09:12 -0300

    adding password reset request and send email

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,20 +1,66 @@
 'use strict'
 
 const crypto = use('crypto')
+const Mail = use('Mail')
+const moment = use('moment')
 const User = use('App/Models/User')
 
 class ForgotPasswordController {
-  async store ({ request, response, auth }) {
+  async store ({ request, response }) {
     try {
       const email = request.input('email')
       const user = await User.findByOrFail('email', email)
+
       user.token = crypto.randomBytes(10).toString('hex')
       user.token_created_at = new Date()
+
       await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password', 'emails.forgot_password-text'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('danyllo.dvs@gmail.com', 'Danyllo | dvsystems')
+            .subject('Recuperação de senha')
+        }
+      )
     } catch (err) {
       return response
         .status(err.status)
         .send({ error: { message: 'Algo não deu certo, esse e-mail existe?' } })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        response
+          .status(401)
+          .send({ error: { message: 'O token de recuperação está expirado' } })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Algo deu errado ao resetar sua senha' } })
     }
   }
 }
